ca39cc7fe5edf95205e87ad5d158f683799c9319
Juampicosta123 <juampicosta81@gmail.com>
2024-10-18 16:37:40 -0300

    finish integrador

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,17 +1,48 @@
 // Render de la vista categorías
 
+import { handleGetProducts } from '../persistence/localStorage'
+import { handleRenderList } from '../views/store'
+
+const handleFilter = (category) => {
+  const products = handleGetProducts()
+
+  if (category === 'Todos') {
+    handleRenderList(products)
+  }
+  if (
+    category === 'Hamburguesas' ||
+    category === 'Papas' ||
+    category === 'Gaseosas'
+  ) {
+    const result = products.filter((product) => product.category === category)
+
+    handleRenderList(result)
+  }
+
+  if (category === 'Mayor Precio') {
+    const result = products.sort((a, b) => b.price - a.price)
+    handleRenderList(result)
+  }
+
+  if (category === 'Menor Precio') {
+    const result = products.sort((a, b) => a.price - b.price)
+    handleRenderList(result)
+  }
+}
+
 export const renderCategories = () => {
   const ulList = document.getElementById('listFilter')
   ulList.innerHTML = `
-    <li id='todos'> Todos los productos </li>
-    <li id='hamburguesas'> Hamburguesas </li>
-    <li id='papas'> Papas </li>
-    <li id='gaseosas'> Gaseosas </li>
-    <li id='mayorPrecio'> Mayor Precio </li>
-    <li id='menorPrecio'> Menos Precio </li>
+    <li id='todos'>Todos</li>
+    <li id='hamburguesas'>Hamburguesas</li>
+    <li id='papas'>Papas</li>
+    <li id='gaseosas'>Gaseosas</li>
+    <li id='mayorPrecio'>Mayor Precio</li>
+    <li id='menorPrecio'>Menor Precio</li>
   `
 
   const liElements = ulList.querySelectorAll('li')
+
   liElements.forEach((li) => {
     li.addEventListener('click', (event) => {
       handleClick(li)
@@ -19,6 +50,8 @@ export const renderCategories = () => {
   })
 
   const handleClick = (element) => {
+    handleFilter(element.innerHTML)
+
     liElements.forEach((li) => {
       if (li.classList.contains('liActive')) {
         li.classList.remove('liActive')
